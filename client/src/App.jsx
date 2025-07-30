312d3f4ca3ac1bb5fecab9a99fc9d4352aeeeb8a
Joel Pallero <esteban.pallero@gmail.com>
2025-07-30 11:33:07 -0300

    Testing home production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,17 @@ import AppRoutes from "@/routes/AppRoutes";
 import Header from "@/layout/Header";
 import Footer from "@/layout/Footer";
 import MenuPopup from "@/components/popups/MenuPopup";
-import useTogglePopup from '@/hooks/useTogglePopup';
+import useTogglePopup from "@/hooks/useTogglePopup";
 
 function App() {
   const { isOpen, togglePopup } = useTogglePopup();
 
-
   return (
-    <BrowserRouter basename="/trompo">
-      <Header onTogglePopup={togglePopup}/>
-      <MenuPopup isOpen={isOpen} onClose={togglePopup}/>
-      <AppRoutes/>
-      <Footer/>
+    <BrowserRouter basename="/">
+      <Header onTogglePopup={togglePopup} />
+      <MenuPopup isOpen={isOpen} onClose={togglePopup} />
+      <AppRoutes />
+      <Footer />
     </BrowserRouter>
   );
 }
