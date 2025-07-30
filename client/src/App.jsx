6ea969344fb4cd5d61ea3c5f8ec663bd0b7ac3b2
Joel Pallero <esteban.pallero@gmail.com>
2025-07-30 10:51:44 -0300

    corrections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/trompo">
       <Header onTogglePopup={togglePopup}/>
       <MenuPopup isOpen={isOpen} onClose={togglePopup}/>
       <AppRoutes/>
