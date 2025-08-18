e0e7dff440096cc3641cbe220e8dae0badc327cf
TrompoAgenciaDev <estebanjoelpallero@hotmail.com>
2025-08-18 00:14:04 -0300

    remove node_modules from repo, add to gitignore

--- client/src/components/services/PageTitle.jsx
+++ client/src/components/services/PageTitle.jsx
@@ -3,10 +3,12 @@ import '../../assets/styles/page-title.css';
 
 function PageTitle({ title, highlight }) {
   return (
-    <section className="full-container diagonal-title bg-yellow">
-      <div className="container page-title">
-        <h1 className="title-page">{title}</h1>
-        <p className="content">{highlight}</p>
+    <section className="full-container bg-yellow diagonal-page-title">
+      <div className="full-container diagonal-title">
+        <div className="container page-title">
+          <h1 className="title-page">{title}</h1>
+          <p className="content">{highlight}</p>
+        </div>
       </div>
     </section>
   );
