07599ca3d99c111e0a6c27198969b9772b7a6f42
TrompoAgenciaDev <estebanjoelpallero@hotmail.com>
2025-09-11 11:18:29 -0300

    php corrections

--- client/public/form-handler.php
+++ client/public/form-handler.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// composer require getbrevo/brevo-php "1.x.x"
+
 header('Content-Type: application/json; charset=utf-8');
 
 // === CARGAR .ENV MANUALMENTE ===
@@ -51,6 +53,7 @@ $payload = [
     ],
     "listIds" => [(int)$listId]
 ];
+//print_r($payload);
 
 // === Enviar a Brevo ===
 $ch = curl_init("https://api.brevo.com/v3/contacts");
@@ -63,6 +66,9 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
 
 $response = curl_exec($ch);
+echo "resultado: ";
+print_r($response);
+echo 'fin resultado';
 
 if ($response === false) {
     echo json_encode([
